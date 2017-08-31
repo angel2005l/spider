0e84241802245a8b7da1aa7e2a4330cd991e420e
huanggy <www.hgy1@163.com>
2017-08-31 17:44:10 +0800

    常见基本目录

---
+++ src/main/java/com/spider/main/SpriderMain.java
@@ -0,0 +1,5 @@
+package com.spider.main;
+
+public class SpriderMain {
+
+}
---
+++ src/test/java/com/spider/test/Mytest.java
@@ -0,0 +1,10 @@
+package com.spider.test;
+
+import org.junit.Test;
+
+public class Mytest {
+	@Test
+	public void test1(){
+		
+	}
+}
